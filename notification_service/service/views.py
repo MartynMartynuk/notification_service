c7c623ce383c05d7fb0bb25a245c247980e30f97
Martyn <ma.ahmatov@tensor.ru>
2022-03-29 17:15:50 +0400

    Add get maillists handlers

--- notification_service/service/views.py
+++ notification_service/service/views.py
@@ -53,8 +53,19 @@ class MailinglistAPIView(ModelViewSet):
     queryset = MailingList.objects.all()
     serializer_class = MailingListSerializer
 
-    def get(self, request):
+    def list(self, request, *args, **kwargs):
+        referer = request.headers['Referer'].split('?')
         mailinglists = MailingList.objects.all()
+        if len(referer) == 2:
+            param_args = {}
+            params = referer[1].split('&')
+            for param in params:
+                param_args[param.split('=')[0]] = param.split('=')[1]
+
+            if param_args.get('maillist_stat') is not None:
+                return Response(self.get_maillist_stat(mailinglists))
+            elif param_args.get('message_stat') is not None:
+                return Response(self.get_message_stat(param_args.get('message_stat')))
         return Response({'mailinglists': MailingListSerializer(mailinglists, many=True).data})
 
     def post(self, request):
@@ -87,6 +98,41 @@ class MailinglistAPIView(ModelViewSet):
             return Response({"complete": "delete mailinglist " + str(pk)})
         except:
             return Response({"error": "Deletion failed"})
+
+    @staticmethod
+    def get_maillist_stat(maillists):
+        ml_tot_number = len(maillists)
+        stats = {'total mailings': ml_tot_number}
+        for maillist in maillists:
+            sent = 0
+            not_sent = 0
+            messages = Message.objects.filter(mailinglist_id=maillist.id)
+            for message in messages:
+                if message.sending_status:
+                    sent += 1
+                else:
+                    not_sent += 1
+            stats['maillist '+str(maillist.id)] = {'messages sent': sent, 'messages not sent': not_sent}
+        return stats
+
+    @staticmethod
+    def get_message_stat(maillist_id):
+        messages = Message.objects.filter(mailinglist_id=maillist_id)
+        maillist = MailingList.objects.get(id=maillist_id)
+        stats = {'maillist client filter': maillist.client_filter,
+                 'total messages': len(messages)}
+        sent = 0
+        not_sent = 0
+        for message in messages:
+            print(message.id)
+            if message.sending_status:
+                sent += 1
+            else:
+                not_sent += 1
+            stats['messages sent'] =  sent
+            stats['messages not sent'] = not_sent
+        print(stats)
+        return stats
 
 
 class MessageAPIView(ModelViewSet):
@@ -127,4 +173,3 @@ class MessageAPIView(ModelViewSet):
             return Response({"complete": "delete message " + str(pk)})
         except:
             return Response({"error": "Deletion failed"})
-
